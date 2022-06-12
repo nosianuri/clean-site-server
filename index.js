b17b451d230b351af9eb4e10a159793efbd1cd26
nosianuri <nurinosia@gmail.com>
2022-06-12 18:03:46 +0600

    use param query body

--- index.js
+++ index.js
@@ -21,6 +21,13 @@ async function run() {
         await client.connect();
         const servicesCollection = client.db("cleanCo").collection("service");
 
+        /*
+      get /get-service => all data
+      post /add-service => create new data
+      put /update-service => modify a data on collection
+      delete /delete-service => delete a data from collection
+    */
+
         app.get("/service", async (req, res) => {
             const services = await servicesCollection.find({}).toArray();
             console.log(services);
@@ -32,6 +39,34 @@ async function run() {
 
 
 run().catch(console.dir);
+
+
+// Body
+
+app.get("/dummy-route/user2", async (req, res) => {
+    const data = req.body;
+  
+    res.json(data);
+  });
+
+
+// Query
+
+app.get("/dummy-route/user", async (req, res) => {
+    const { name, age } = req.query;
+    console.log(name);
+    console.log(age);
+    res.json(name);
+  });
+
+
+// Param
+
+app.get("/dummy-route/user/:id", async (req, res) => {
+    const { id } = req.params;
+  
+    res.json(id);
+  });
 
 
 app.get("/", async (req, res) => {
